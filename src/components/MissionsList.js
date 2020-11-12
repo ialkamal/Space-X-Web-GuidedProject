52a8cf02578c581d811cb8946d8ff318f7c937aa
Ismail Al Kamal <ismail@nawatt.com>
2020-11-12 14:19:31 +0300

    Completed Test Project

--- src/components/MissionsList.js
+++ src/components/MissionsList.js
@@ -1,14 +1,20 @@
 import React from "react";
 
-const MissionsList = props => {
+const MissionsList = (props) => {
   return (
     <section className="missions-list">
       {props.error ? (
-        <div className="error">{props.error}</div>
+        <div data-testid="error" className="error">
+          {props.error}
+        </div>
       ) : (
         <div>
-          {props.missions.map(mission => (
-            <div className="mission" key={mission.mission_id}>
+          {props.missions.map((mission) => (
+            <div
+              data-testid="mission"
+              className="mission"
+              key={mission.mission_id}
+            >
               {mission.mission_name}
             </div>
           ))}
